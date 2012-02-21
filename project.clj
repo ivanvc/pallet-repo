f177014a15cd5c4822fc4e79b40c3faba7b449f0
Ivan Valdes (@ivanvc) <iv@nvald.es>
2012-02-20 23:22:25 -0600

    Initial set of configs

--- project.clj
+++ project.clj
@@ -2,8 +2,11 @@
             :description "FIXME: write"
             :dependencies [[org.clojure/clojure "1.2.0"]
                            [org.clojure/clojure-contrib "1.2.0"]
-                           [org.cloudhoist/pallet "0.6.6"]
-                           [org.cloudhoist/pallet-crates-standalone "0.5.0"]
+                           [org.cloudhoist/pallet "0.6.7"]
+                           [org.cloudhoist/ruby "0.6.0"]
+                           [org.cloudhoist/git "0.5.0"]
+                           [org.cloudhoist/nginx "0.5.0"]
+                           [org.cloudhoist/automated-admin-user "0.6.0"]
                            [org.slf4j/slf4j-api "1.6.1"]
                            [ch.qos.logback/logback-core "1.0.0"]
                            [ch.qos.logback/logback-classic "1.0.0"]
@@ -11,4 +14,6 @@
                            [org.jclouds.driver/jclouds-jsch "1.0.0"]
                            [org.jclouds.driver/jclouds-slf4j "1.0.0"]]
             :repositories {"sonatype"
-                           "http://oss.sonatype.org/content/repositories/releases"})
+                           "http://oss.sonatype.org/content/repositories/releases"
+                           "sonatype-snapshots"
+                           "http://oss.sonatype.org/content/repositories/snapshots"})
--- src/pallet_repo/core.clj
+++ src/pallet_repo/core.clj
@@ -1,1 +1,27 @@
-(ns pallet-repo.core)
+(ns pallet-repo.core
+  (:require pallet.repl)
+  (:use
+    [pallet.phase :only [phase-fn]]
+    [pallet.utils :only [make-user]]
+    [pallet.crate.automated-admin-user :only [automated-admin-user]]
+    [pallet.crate.git :only [git]]
+    [pallet.crate.ruby :only [ruby]]
+    [pallet.crate.nginx :only [nginx]]))
+
+(pallet.repl/use-pallet)
+
+(def local
+  (pallet.compute/compute-service
+    "node-list"
+    :node-list [["vagrant" "vagrant" "33.33.33.10" :ubuntu]]))
+
+(def vagrant-user
+  (make-user "vagrant" :password "vagrant"))
+
+(def ps-front
+  (group-spec "vagrant"
+              :phases {:bootstrap (phase-fn (automated-admin-user))
+                       :configure (phase-fn
+                                    (git)
+                                    (nginx :version "1.0.12")
+                                    (ruby "1.9.3-p125"))}))
